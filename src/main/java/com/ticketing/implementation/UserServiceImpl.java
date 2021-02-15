9ad9fbaf87d47ece4c540c19057c68c29d39c76f
memintan <memintan@gmail.com>
2021-02-14 23:06:01 -0600

    Binding Ticketing MVC to ORM

--- src/main/java/com/ticketing/implementation/UserServiceImpl.java
+++ src/main/java/com/ticketing/implementation/UserServiceImpl.java
@@ -1,16 +1,34 @@
 package com.ticketing.implementation;
 
 import com.ticketing.dto.UserDTO;
+import com.ticketing.entitiy.User;
+import com.ticketing.mapper.UserMapper;
+import com.ticketing.repository.UserRepository;
 import com.ticketing.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+
+    UserRepository userRepository;
+    UserMapper userMapper;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
     @Override
     public List<UserDTO> listAllUsers() {
-        return null;
+        List<User> list = userRepository.findAll(Sort.by("firstName"));
+
+        return list.stream().map(obj -> {return userMapper.convertToDto(obj);}).collect(Collectors.toList());
     }
 
     @Override
